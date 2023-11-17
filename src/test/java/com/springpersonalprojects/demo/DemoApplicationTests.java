a9f8a8b611298a2e57c947192c0477139ad1ae5d
Jeremiah Matthews <jeremiah.matthews105@gmail.com>
2023-11-17 07:55:07 -0600

    first commit

---
+++ src/test/java/com/springpersonalprojects/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springpersonalprojects.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
